1258a53d113aa837ea2af3e316bf16845335867e
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2026-06-05 15:56:07 +0530

    Implement maxArea function using greedy method

---
+++ 11ContainerWithMostWater.cpp
@@ -0,0 +1,28 @@
+//we are going to use greedy method
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0;
+        int j=height.size()-1;
+        int maxx=0;
+        while(i<j){
+            int w=j-i;
+            int h= min(height[i],height[j]);
+            int area= w*h;
+            maxx= max(maxx,area);
+            if(height[i]>height[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+
+        }
+
+
+
+
+        return maxx;
+    }
+};
